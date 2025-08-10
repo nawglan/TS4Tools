459335302deea7afe1e86224f923cd245eda918f
Desmond Daignault <nawglan@users.noreply.github.com>
2025-08-10 19:02:43 -0400

    feat(test): add copyright headers to GlobalUsings.cs files
    - Add GPL v3 copyright header to Effects.Tests GlobalUsings.cs
    - Add GPL v3 copyright header to Visual.Tests GlobalUsings.cs
    - Include NSubstitute import for mocking support
    
    WHY: Ensure copyright compliance and consistency with project standards
    TECHNICAL IMPACT: Standardizes legal headers across all test project files

--- tests/TS4Tools.Resources.Effects.Tests/GlobalUsings.cs
+++ tests/TS4Tools.Resources.Effects.Tests/GlobalUsings.cs
@@ -1,3 +1,21 @@
+/***************************************************************************
+ * TS4Tools - A comprehensive toolkit for working with The Sims 4 packages
+ * Copyright (C) 2024 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics;
@@ -9,6 +27,7 @@ global using System.Threading.Tasks;
 global using FluentAssertions;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
+global using NSubstitute;
 global using Xunit;
 global using TS4Tools.Core.Interfaces;
 global using TS4Tools.Resources.Effects;
